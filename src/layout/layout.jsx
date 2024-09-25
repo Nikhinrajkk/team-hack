3689910105fb67925e2e3460ee4eaea3edb442bf
Nikhin <nikhinrajkk@gmail.com>
2024-09-25 19:15:52 +0530

    Add animated background elements and refactor layout component

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,41 +1,14 @@
-import { useEffect } from "react";
-
 import Landing from "../pages/login";
 
 const Layout = () => {
-  useEffect(() => {
-    animateDiv(".ball");
-
-    function makeNewPosition() {
-      // Get viewport dimensions (remove the dimension of the div)
-      var h = window.innerHeight - 50;
-      var w = window.innerWidth - 50;
-
-      var nh = Math.floor(Math.random() * h);
-      var nw = Math.floor(Math.random() * w);
-
-      return [nh, nw];
-    }
-
-    function animateDiv(myclass) {
-      var newq = makeNewPosition();
-      var element = document.querySelector(myclass);
-
-      element.style.transition = "top 1s, left 1s";
-      element.style.top = newq[0] + "px";
-      element.style.left = newq[1] + "px";
-      element.style.transitionDuration = "5s";
-
-      console.log(newq);
-      setTimeout(function () {
-        animateDiv(myclass);
-      }, 3000);
-    }
-  }, []);
   return (
-    <div className="app-container">
+    <div className="">
       <div className="glow-container">
         <div className="ball" />
+        <div className="ball1"></div>
+        <div className="ball2"></div>
+      </div>
+      <div className="w-screen h-screen flex items-center justify-center z-10">
         <Landing />
       </div>
     </div>
