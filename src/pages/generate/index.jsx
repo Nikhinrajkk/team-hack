15d753a6c43e6d572aaaa8d393cf605105843610
Nikhin <nikhinrajkk@gmail.com>
2024-09-26 17:14:45 +0530

    Add Poppins font and generate question page with routing

---
+++ src/pages/generate/index.jsx
@@ -0,0 +1,8 @@
+const GenerateQuestion = () => (
+  <div className="flex items-center justify-center h-screen w-screen poppins">
+    <div className="text-[40px]">Generate your question paper.</div>
+    <div></div>
+  </div>
+);
+
+export default GenerateQuestion;
