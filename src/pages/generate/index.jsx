45f45523b928d7302bc8559c361fe58cea2a5978
Nikhin <nikhinrajkk@gmail.com>
2024-09-26 17:19:51 +0530

    Add Clash Display font and enhance question generation page layout

--- src/pages/generate/index.jsx
+++ src/pages/generate/index.jsx
@@ -1,7 +1,15 @@
 const GenerateQuestion = () => (
   <div className="flex items-center justify-center h-screen w-screen poppins">
-    <div className="text-[40px]">Generate your question paper.</div>
-    <div></div>
+    <div>
+      <div className="text-[40px] font-normal">
+        Generate your question paper.
+      </div>
+      <div className="text-[20px] font-light mt-5">
+        Mars offers personalised tutoring based on a calculated
+        <br />
+        assessment of your strong and weak points
+      </div>
+    </div>
   </div>
 );
 
